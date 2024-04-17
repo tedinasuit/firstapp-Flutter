bdf569c5e50db2f3e918b3854c6352e528591a65
Lars Hoeijmans <i474593@fhict.nl>
2024-04-17 21:38:27 +0200

    learned more
    box + box color + centered it

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,16 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter')
         ),
 
-      body: Container(
-        child: const Text('Test text'),
-        margin: const EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        )
+      body: Center(
+        child: Container(
+          child: const Text('Test text'),
+          margin: const EdgeInsets.all(100),
+          padding: EdgeInsets.all(10),
+          height: 100,
+          width: 100,
+          color: Colors.blueGrey,
+          ),
+      )
 
       ),
     );
