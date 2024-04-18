5a78a8a65605c6a6297f6163195baf82a2bc7199
Lars Hoeijmans <i474593@fhict.nl>
2024-04-18 22:00:06 +0200

    Instructions button works!
    Instructions button now takes you to Instructions page

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp(MyApp());
 }
 
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   int count = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 255, 90, 90),
-          title: const Text('Eindhoven Freedom Walk')
-        ),
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 90, 90),
+        title: const Text('Eindhoven Freedom Walk'),
+      ),
       body: Column(
         children: [
-          Center (
+          Center(
             child: Text(
               '$count',
               style: const TextStyle(fontSize: 50),
             ),
           ),
-
           ElevatedButton(
             onPressed: () {
               Navigator.push(
-                context, 
+                context,
                 MaterialPageRoute(
-                  builder: (_) => const Instructions(),
-                )
-                
+                  builder: (_) => Instructions(),
+                ),
               );
-            }, 
-            child: const Text('Instructions')
-            ),
+            },
+            child: const Text('Instructions'),
+          ),
         ],
       ),
-
-      floatingActionButton: FloatingActionButton (
+      floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromARGB(255, 255, 90, 90),
-        child: const Icon(Icons.add), 
+        child: const Icon(Icons.add),
         onPressed: () {
           setState(() {
             count++;
           });
-        }
+        },
       ),
-      bottomNavigationBar: BottomNavigationBar (
+      bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_filled),
             label: 'Home',
           ),
-                    BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.leaderboard),
             label: 'Ranking',
           ),
-                    BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
           ),
         ],
-      ),
-
-    
-
       ),
     );
   }
 }
 
 class Instructions extends StatelessWidget {
-  const Instructions({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Instructions'),
+      ),
+      body: const Center(
+        child: Text('Instructions here'),
+      ),
     );
   }
 }
-
